6047eee2e787dee539d3fe0666cb24cbfe699d26
EmDash00 <emberchow00@gmail.com>
2025-01-04 14:48:18 -0800

    update github workflow for 2025

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Template-Robot-Code-Repo-Imported";
+  public static final String MAVEN_NAME = "2025_Season";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 9;
-  public static final String GIT_SHA = "8bf8219a3e49ce6276bf4744f14f68b621084215";
-  public static final String GIT_DATE = "2025-01-04 15:54:45 EST";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "d7d4c3ea7b36874c82243bd3f22ea7c9e6c92b3a";
+  public static final String GIT_DATE = "2025-01-04 17:44:34 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-04 17:03:07 EST";
-  public static final long BUILD_UNIX_TIME = 1736028187205L;
+  public static final String BUILD_DATE = "2025-01-04 17:47:16 EST";
+  public static final long BUILD_UNIX_TIME = 1736030836681L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
