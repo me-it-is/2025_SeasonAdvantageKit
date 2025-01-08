b0d749f98abf5f14e0b543c8a6b2cac5e0e7542b
TurtleMeds <owenwells2009@gmail.com>
2025-01-07 16:22:05 -0800

    update pathplannerlib from beta

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_Season";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "6047eee2e787dee539d3fe0666cb24cbfe699d26";
-  public static final String GIT_DATE = "2025-01-04 17:48:18 EST";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "f9884408afc7a98100c9be60514ef1d1904b57ea";
+  public static final String GIT_DATE = "2025-01-07 19:06:59 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-07 19:06:17 EST";
-  public static final long BUILD_UNIX_TIME = 1736294777097L;
+  public static final String BUILD_DATE = "2025-01-07 19:21:14 EST";
+  public static final long BUILD_UNIX_TIME = 1736295674223L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
