398ca8c832d12d2bdf9b0fffb2e1a91a6ad84559
Rafferty <raffertymclain@gmail.com>
2025-01-11 13:19:18 -0800

    added superstructure

---
+++ src/main/java/frc/robot/Superstructure.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Superstructure extends SubsystemBase {
+  // Initalize subsystems
+
+  public Superstructure() {}
+
+  // commands
+
+}
