d4f5131e7269acf8839f5c4a838bcbc1e1fd2a58
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2025-01-12 21:04:02 -0800

    vision multi camera localization, added style.yml

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,7 +13,22 @@
 
 package frc.robot;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.RobotBase;
+import java.util.List;
+import java.util.Map;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
 /**
  * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
@@ -42,5 +57,52 @@ public final class Constants {
 
   public static class DriveConstants {
     public static final double DRIVE_DEADBAND = 0.02;
+    public static final PIDController translationController = new PIDController(0.4, 0.001, 0);
+    public static final PIDController rotationController = new PIDController(0.3, 0, 0);
+  }
+
+  public static class VisionConstants {
+    public static final double kFieldWidthMeters = 16.54;
+    // TODO set to correct values, in meters
+    public static final double camChassisXOffset = 0.3;
+    public static final double camChassisYOffset = 0.3;
+    public static final double camChassisZOffset = 0.0;
+
+    public static final double tagDistSetpoint = 0.0;
+    public static final double kCameraHeight = 0.1;
+    public static final double kCameraPitchRadians = 0;
+    public static final Transform3d robotToCam =
+        new Transform3d(
+            new Translation3d(camChassisXOffset, camChassisYOffset, camChassisZOffset),
+            new Rotation3d(0, 0, 0));
+
+    public static final Matrix<N3, N1> kMultiTagStdDevs =
+        VecBuilder.fill(0.5, 0.5, Double.POSITIVE_INFINITY);
+
+    public static final AprilTagFieldLayout aprilTagFieldLayout =
+        AprilTagFields.kDefaultField.loadAprilTagLayoutField();
+
+    public static final PhotonCamera aprilCamOne = new PhotonCamera("aprilOne");
+    public static final PhotonCamera aprilCamTwo = new PhotonCamera("aprilTwo");
+
+    public static final PhotonPoseEstimator poseEstimatorOne =
+        new PhotonPoseEstimator(
+            aprilTagFieldLayout,
+            PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+            VisionConstants.robotToCam);
+    public static final PhotonPoseEstimator poseEstimatorTwo =
+        new PhotonPoseEstimator(
+            aprilTagFieldLayout,
+            PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+            VisionConstants.robotToCam); // TODO need diff config for second camera
+
+    // height of bottom of april tag off floor in meters
+    public static final Map<List<Integer>, Double> tagToHeight =
+        Map.of(
+            List.of(1, 2, 12, 13), 1.40335, // coral station
+            List.of(3, 16), 1.216152, // processor
+            List.of(4, 5, 14, 15), 1.796542, // barge
+            List.of(6, 7, 8, 9, 10, 11, 17, 18, 19, 20, 21, 22), 0.22225 // reef
+            );
   }
 }
---
+++ src/main/java/frc/robot/Superstructure.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Superstructure extends SubsystemBase {
+
+  public Superstructure() {}
+
+  // commands
+
+}
