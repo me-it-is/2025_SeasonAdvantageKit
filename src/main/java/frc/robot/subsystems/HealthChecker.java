23325967f9559853ecd549b67689b346881b2a16
Rafferty <raffertymclain@gmail.com>
2025-01-21 15:01:09 -0800

    Added info on what check health should do

--- src/main/java/frc/robot/subsystems/HealthChecker.java
+++ src/main/java/frc/robot/subsystems/HealthChecker.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems;
 
 public interface HealthChecker {
+  /*
+   * The checkHealth function should return a boolean saying if there are any faults in the subsystem
+   * it should also if any faults are found create a notifaction on the elastic dashboard with the faults that are detected
+   */
   public boolean checkHealth();
 }
