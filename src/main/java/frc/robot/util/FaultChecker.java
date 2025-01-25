10b29ad7c5544d820f93723592e5a5c9ae883c39
Rafferty <raffertymclain@gmail.com>
2025-01-24 16:46:24 -0800

    moved fault from faultChecker

--- src/main/java/frc/robot/util/FaultChecker.java
+++ src/main/java/frc/robot/util/FaultChecker.java
@@ -2,8 +2,6 @@ package frc.robot.util;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
-import frc.robot.util.Elastic.Notification.NotificationLevel;
 import frc.robot.util.Elastic.Notification;
 
 public class FaultChecker {
@@ -12,39 +10,6 @@ public class FaultChecker {
 
     public FaultChecker(String subsytemName) {
         this.subsystemName = subsytemName;
-    }
-
-    public class Fault {
-        String faultName;
-        Function<Boolean, Boolean> functionToCheckFault;
-        NotificationLevel level = NotificationLevel.WARNING;
-        boolean hasFault;
-
-        public Fault(Function<Boolean, Boolean> functionToCheckFault) {
-            this.functionToCheckFault = functionToCheckFault;
-            faultName = functionToCheckFault.toString().replace("getFault_", "");
-        }
-
-        public Fault(Function<Boolean, Boolean> functionToCheckFault, String name) {
-            this.functionToCheckFault = functionToCheckFault;
-            faultName = name;
-        }
-
-        public Fault(Function<Boolean, Boolean> functionToCheckFault, NotificationLevel notificationLevel) {
-            this.functionToCheckFault = functionToCheckFault;
-            faultName = functionToCheckFault.toString().replace("getFault_", "");
-            level = notificationLevel;
-        }
-
-        public Fault(Function<Boolean, Boolean> functionToCheckFault, String name, NotificationLevel notificationLevel) {
-            this.functionToCheckFault = functionToCheckFault;
-            faultName = name;
-            level = notificationLevel;
-        }
-        
-        public void updateFault() {
-            hasFault = functionToCheckFault.apply(true);
-        }
     }
 
     public void updateFaults() {
