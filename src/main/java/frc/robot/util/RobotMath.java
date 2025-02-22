d255e799cf13c41e4f03aecb5854931adf3d7870
Rafferty <raffertymclain@gmail.com>
2025-02-22 12:11:03 -0800

    add tolerance checks and supress warnings

--- src/main/java/frc/robot/util/RobotMath.java
+++ src/main/java/frc/robot/util/RobotMath.java
@@ -30,6 +30,7 @@ public final class RobotMath {
    * @param other measuere to get dist to.
    * @return abs of measuere1 - measuere2.
    */
+  @SuppressWarnings("unchecked")
   public static <U extends Unit, M extends Measure<U>> M dist(M measure1, M measure2) {
     return abs((M) measure1.minus(measure2));
   }
@@ -38,7 +39,16 @@ public final class RobotMath {
    * @param measure measure to get the absolute value of.
    * @return the absulute value of measure with the zero point of the base unit for that type.
    */
+  @SuppressWarnings("unchecked")
   public static <U extends Unit, M extends Measure<U>> M abs(M measure) {
     return measure.gt((M) measure.baseUnit().zero()) ? measure : (M) measure.unaryMinus();
+  }
+
+  public static <U extends Unit> boolean isWithinTolerance(Measure<U> measure1, Measure<U> measure2, Measure<U> tol) {
+    return dist(measure1, measure2).lte(tol);
+  }
+  @SuppressWarnings("unchecked")
+  public static <U extends Unit> boolean approxZero(Measure<U> measure) {
+    return abs(measure).lte((Measure<U>) measure.baseUnit().of(1e-6));
   }
 }
