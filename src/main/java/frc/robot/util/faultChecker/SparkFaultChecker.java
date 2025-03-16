9a17ae354106ddfbce7bfb49ace0e4c54bbee559
Rafferty <raffertymclain@gmail.com>
2025-03-15 22:18:44 -0700

    update SparkFaultChecker to have a local copy of spark faults

--- src/main/java/frc/robot/util/faultChecker/SparkFaultChecker.java
+++ src/main/java/frc/robot/util/faultChecker/SparkFaultChecker.java
@@ -1,7 +1,20 @@
 package frc.robot.util.faultChecker;
 
+import java.util.function.Supplier;
+
+import com.revrobotics.spark.SparkBase.Faults;
+
 public class SparkFaultChecker extends AbstractFaultChecker {
-  public SparkFaultChecker(String deviceName) {
+  private Supplier<Faults> faultSupplier;
+  public Faults faults;
+
+  public SparkFaultChecker(Supplier<Faults> faultSupplier, String deviceName) {
     super(deviceName);
+    this.faultSupplier = faultSupplier;
+  }
+
+  public void updateFaults() {
+    faults = faultSupplier.get();
+    super.updateFaults();
   }
 }
