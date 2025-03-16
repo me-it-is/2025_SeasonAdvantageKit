1836464d439584a55e5fd94bcdc2f60103357b10
Rafferty <raffertymclain@gmail.com>
2025-03-16 09:38:26 -0700

    add docs to CTREFault checker

--- src/main/java/frc/robot/util/faultChecker/CTREFaultChecker.java
+++ src/main/java/frc/robot/util/faultChecker/CTREFaultChecker.java
@@ -6,20 +6,36 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import frc.robot.Constants.FaultConstants;
 
 public class CTREFaultChecker extends AbstractFaultChecker {
+
+  /**
+   * @param deviceName Name of the component to fault check.
+   */
   public CTREFaultChecker(String deviceName) {
     super(deviceName);
   }
 
+  /**
+   * @param device Talon to check faults on.
+   * @param deviceName Name of the component to fault check.
+   */
   public CTREFaultChecker(TalonFX device, String deviceName) {
     super(deviceName);
     super.addFaults(FaultConstants.getTalonFXFaults(device));
   }
 
+  /**
+   * @param device Pigeon to check faults on.
+   * @param deviceName Name of the component to fault check.
+   */
   public CTREFaultChecker(Pigeon2 device, String deviceName) {
     super(deviceName);
     super.addFaults(FaultConstants.getPigeonFaults(device));
   }
 
+  /**
+   * @param device CANcoder to check faults on.
+   * @param deviceName Name of the component to fault check.
+   */
   public CTREFaultChecker(CANcoder device, String deviceName) {
     super(deviceName);
     super.addFaults(FaultConstants.getCANCoderFaults(device));
