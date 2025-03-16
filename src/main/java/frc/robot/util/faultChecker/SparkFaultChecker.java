4357054e4f9f574ab5154e5fc0eba1399695d9b1
Rafferty <raffertymclain@gmail.com>
2025-03-16 09:45:38 -0700

    add docs to SparkFaultChecker

--- src/main/java/frc/robot/util/faultChecker/SparkFaultChecker.java
+++ src/main/java/frc/robot/util/faultChecker/SparkFaultChecker.java
@@ -7,17 +7,27 @@ import java.util.function.Supplier;
 public class SparkFaultChecker extends AbstractFaultChecker {
   private Supplier<Faults> faultSupplier;
   public Faults faults;
-
+  /**
+   * @param faultSupplier Supplier or faults for the device.
+   * @param deviceName Name of the device to check faults on.
+   */
   public SparkFaultChecker(Supplier<Faults> faultSupplier, String deviceName) {
     super(deviceName);
     this.faultSupplier = faultSupplier;
   }
-
+  /**
+   * @param device SparkMax or SparkFlex to check faults on.
+   * @param deviceName Name of the device to check faults on.
+   */
   public <U extends SparkBase> SparkFaultChecker(U device, String deviceName) {
     super(deviceName);
     this.faultSupplier = () -> (device.getFaults());
   }
 
+  /**
+   * Updates all faults in the fault checker. If any faults have been resolved or stated it will log
+   * or report them based on sevarity
+   */
   public void updateFaults() {
     faults = faultSupplier.get();
     super.updateFaults();
