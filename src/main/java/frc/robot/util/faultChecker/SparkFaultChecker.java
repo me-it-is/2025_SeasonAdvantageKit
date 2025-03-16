d66f29af06e24da8c4b8f14c82df3566da04e3be
Rafferty <raffertymclain@gmail.com>
2025-03-15 21:44:43 -0700

    add SparkFaultChecker

---
+++ src/main/java/frc/robot/util/faultChecker/SparkFaultChecker.java
@@ -0,0 +1,7 @@
+package frc.robot.util.faultChecker;
+
+public class SparkFaultChecker extends FaultChecker {
+    public SparkFaultChecker (String deviceName) {
+        super(deviceName);
+    }
+}
