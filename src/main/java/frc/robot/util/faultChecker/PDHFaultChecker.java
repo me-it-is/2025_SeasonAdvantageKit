35dc7faf5dab061ff34dbc92719441feef50e559
Rafferty <raffertymclain@gmail.com>
2025-03-16 20:01:09 -0700

    add PDHFaultChecker

---
+++ src/main/java/frc/robot/util/faultChecker/PDHFaultChecker.java
@@ -0,0 +1,7 @@
+package frc.robot.util.faultChecker;
+
+public class PDHFaultChecker extends AbstractFaultChecker{
+    public PDHFaultChecker(String deviceName) {
+        super(deviceName);
+    }
+}
