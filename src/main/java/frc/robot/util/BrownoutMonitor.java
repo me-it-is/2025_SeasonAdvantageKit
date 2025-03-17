9587dcfa5cc1e66e72b7872aabc565b122f2c0c2
Rafferty <raffertymclain@gmail.com>
2025-03-16 20:32:02 -0700

    implement PDHFaultChecker in BrownoutMonitor as well as remove unsuported faults.

--- src/main/java/frc/robot/util/BrownoutMonitor.java
+++ src/main/java/frc/robot/util/BrownoutMonitor.java
@@ -2,7 +2,6 @@ package frc.robot.util;
 
 import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Celsius;
-import static edu.wpi.first.units.Units.Joules;
 import static edu.wpi.first.units.Units.Volts;
 
 import edu.wpi.first.wpilibj.PowerDistribution;
@@ -13,8 +12,7 @@ import frc.robot.subsystems.climber.Climber;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.manipulator.Manipulator;
-import frc.robot.util.Elastic.Notification;
-import frc.robot.util.Elastic.Notification.NotificationLevel;
+import frc.robot.util.faultChecker.PDHFaultChecker;
 import monologue.Logged;
 
 public class BrownoutMonitor extends SubsystemBase implements Logged {
@@ -24,12 +22,16 @@ public class BrownoutMonitor extends SubsystemBase implements Logged {
   private Elevator elevator;
   private Manipulator manipulator;
 
+  private PDHFaultChecker faultChecker;
+
   public BrownoutMonitor(Drive drive, Climber climber, Elevator elevator, Manipulator manipulator) {
     this.robotPower = new PowerDistribution(1, ModuleType.kRev);
     this.drive = drive;
     this.climber = climber;
     this.elevator = elevator;
     this.manipulator = manipulator;
+
+    faultChecker = new PDHFaultChecker(robotPower, "PDH");
   }
 
   @Override
@@ -37,46 +39,18 @@ public class BrownoutMonitor extends SubsystemBase implements Logged {
     this.log("pdh voltage", robotPower.getVoltage());
     this.log("pdh current", robotPower.getAllCurrents());
 
-    /*if (robotPower.getFaults() != null) {
-      Elastic.sendNotification(
-          new Notification(
-              NotificationLevel.WARNING, "PDH faults", robotPower.getFaults().toString()));
-    }*/
+    faultChecker.updateFaults();
 
     boolean shutdown = false;
     if (Amps.of(robotPower.getTotalCurrent()).gte(PDHConstants.kMaxTotalCurrentDraw)) {
-      Elastic.sendNotification(
-          new Notification(
-              NotificationLevel.WARNING,
-              "too high PDH current draw",
-              Double.toString(robotPower.getTotalCurrent())));
       shutdown = true;
     }
 
     if (Volts.of(robotPower.getVoltage()).gte(PDHConstants.kMaxInputVoltage)) {
-      Elastic.sendNotification(
-          new Notification(
-              NotificationLevel.WARNING,
-              "too high PDH input voltage",
-              Double.toString(robotPower.getVoltage())));
-      shutdown = true;
-    }
-
-    if (Joules.of(robotPower.getTotalEnergy()).gte(PDHConstants.kBatteryEnergy)) {
-      Elastic.sendNotification(
-          new Notification(
-              NotificationLevel.WARNING,
-              "too fast battery drain",
-              Double.toString(robotPower.getTotalEnergy())));
       shutdown = true;
     }
 
     if (Celsius.of(robotPower.getTemperature()).gte(PDHConstants.kMaxTemperature)) {
-      Elastic.sendNotification(
-          new Notification(
-              NotificationLevel.WARNING,
-              "too high PDH temperature",
-              Double.toString(robotPower.getTemperature())));
       shutdown = true;
     }
 
