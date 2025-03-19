51e834cb848d365b1d0b0ae146d3afcce1407207
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2025-03-19 15:18:04 -0700

    w/o motion profile dont break plz

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -1,20 +1,15 @@
 package frc.robot.subsystems.elevator;
 
 import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.Rotations;
-import static frc.robot.Constants.ElevatorConstants.*;
 
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkClosedLoopController.ArbFFUnits;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+
+import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -30,15 +25,7 @@ public class Elevator extends SubsystemBase implements AutoCloseable, Logged {
   private SparkFaultChecker followerChecker;
   private RelativeEncoder encoder;
   public SparkClosedLoopController pidControllerLeader;
-  SparkMaxConfig globalConfig;
-  SparkMaxConfig followerConfig;
-  SparkMaxConfig leaderConfig;
-  private Distance setpoint;
-  private TrapezoidProfile profile;
-  private State m_goal;
-  private State m_prevGoal;
-  private State profileSetpoint;
-  private static double kDt = 0.02;
+  private Distance setpoint = Constants.reefMap.get(GameState.NONE).distance();
 
   public Elevator(SparkMax sparkMaxLeader, SparkMax sparkMaxFollower) {
     this.sparkMaxLeader = sparkMaxLeader;
@@ -47,68 +34,45 @@ public class Elevator extends SubsystemBase implements AutoCloseable, Logged {
     this.followerChecker = new SparkFaultChecker(sparkMaxFollower, "elevator follower");
     this.encoder = sparkMaxLeader.getEncoder();
     this.pidControllerLeader = sparkMaxLeader.getClosedLoopController();
-    this.setpoint = Constants.reefMap.get(GameState.NONE).distance();
-    this.profile = getProfile();
-    this.m_goal = new TrapezoidProfile.State();
-    this.m_prevGoal = new TrapezoidProfile.State();
-    this.profileSetpoint = new TrapezoidProfile.State();
 
     encoder.setPosition(0);
     sparkMaxLeader.configure(
-        getLeaderConfig(), ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        ElevatorConstants.getLeaderConfig(), ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     sparkMaxFollower.configure(
-        getFollowerConfig(), ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        ElevatorConstants.getFollowerConfig(), ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
+
 
   @Override
   public void periodic() {
     this.log("elevator/height meters", getElevatorHeight().in(Meters));
-    this.log("elevator/setpoint meters", setpoint.in(Meters));
+    this.log("elevator/setpoint meters", setpoint.in(Units.Meters));
     this.log("elevator/leader output", sparkMaxLeader.get());
     this.log("elevator/follower output", sparkMaxFollower.get());
+  }
 
-    if (!m_prevGoal.equals(m_goal)) {
-      System.out.println("reinitializing profile");
-      profile =
-          ElevatorConstants
-              .getProfile(); // reinitialize the profile to calculate new motion trajectory
-      m_prevGoal = new State(m_goal.position, m_goal.velocity);
-    }
-
-    profileSetpoint = profile.calculate(kDt, profileSetpoint, m_goal);
-    this.log("elevator/profile setpoint pos", profileSetpoint.position);
-    this.log("elevator/profile setpoint vel", profileSetpoint.velocity);
-    pidControllerLeader.setReference(
-        profileSetpoint.position,
-        ControlType.kPosition,
-        ClosedLoopSlot.kSlot0,
-        ElevatorConstants.kFF * profileSetpoint.velocity,
-        ArbFFUnits.kPercentOut);
+  public void setSetpoint(GameState stage) {
+    this.setpoint = Constants.reefMap.get(stage).distance();
+    pidControllerLeader.setReference(metersToRots(setpoint.in(Meters)), ControlType.kPosition);
 
     leaderChecker.updateFaults();
     followerChecker.updateFaults();
   }
 
-  public void setSetpoint(GameState stage) {
-    this.setpoint = Constants.reefMap.get(stage).distance();
-    double setpointConv =
-        setpoint.in(Meters)
-            / ElevatorConstants.kMaxHeight.in(Meters)
-            * ElevatorConstants.kRotsPerFullExtension;
-    this.m_goal = new TrapezoidProfile.State(setpointConv, 0);
-  }
-
   public boolean atSetpoint() {
-    return Math.abs(setpoint.in(Meters) - getElevatorHeight().in(Meters))
-            < ElevatorConstants.kSetpointTolerance.in(Meters)
-        && Math.abs(profileSetpoint.velocity) < ElevatorConstants.rotVelTolerance.in(Rotations);
+    return Math.abs(setpoint.in(Meters) - getElevatorHeight().in(Meters)) < ElevatorConstants.kSetpointTolerance.in(Meters);
   }
 
   public Distance getElevatorHeight() {
-    return Meters.of(
-        encoder.getPosition()
-            / ElevatorConstants.kRotsPerFullExtension
-            * ElevatorConstants.kMaxHeight.in(Meters));
+    return Meters.of(rotsToMeters(encoder.getPosition()));
+  }
+
+  private double rotsToMeters(double rots) {
+    return rots / ElevatorConstants.kRotsPerFullExtension * ElevatorConstants.kMaxHeight.in(Meters);
+  }
+
+  private double metersToRots(double meters) {
+    return meters / ElevatorConstants.kMaxHeight.in(Meters) * ElevatorConstants.kRotsPerFullExtension;
   }
 
   public void stop() {
