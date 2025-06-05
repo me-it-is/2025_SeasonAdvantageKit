667b852e0f0303114e33c15db6ceb3af8ec4e4df
Me-it-is <188520719+me-it-is@users.noreply.github.com>
2025-06-05 08:12:16 -0700

    up path planning strength

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -130,7 +130,7 @@ public class Drive extends SubsystemBase {
         this::getChassisSpeeds,
         this::runVelocity,
         new PPHolonomicDriveController(
-            new PIDConstants(0.5, 0.0, 0.0), new PIDConstants(0.5, 0.0, 0.0)),
+            new PIDConstants(5, 0.0, 0.0), new PIDConstants(5, 0.0, 0.0)),
         PP_CONFIG,
         () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red,
         this);
