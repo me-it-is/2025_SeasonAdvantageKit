c205ac6e930c563b2cfdcc58adfbe876a4adbd85
me-it-is <188520719+me-it-is@users.noreply.github.com>
2025-07-01 01:29:58 -0700

    fix style, add missing methods, change manipulator to use inputs

--- src/main/java/frc/robot/subsystems/manipulator/Manipulator.java
+++ src/main/java/frc/robot/subsystems/manipulator/Manipulator.java
@@ -3,88 +3,34 @@ package frc.robot.subsystems.manipulator;
 import static edu.wpi.first.units.Units.Rotations;
 import static frc.robot.Constants.ManipulatorConstants.*;
 
-import com.revrobotics.AbsoluteEncoder;
-import com.revrobotics.spark.ClosedLoopSlot;
-import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkClosedLoopController.ArbFFUnits;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
 import dev.doglog.DogLog;
-import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.GameState;
-import frc.robot.Constants.ManipulatorConstants;
 import frc.robot.util.RobotMath;
-import frc.robot.util.faultChecker.SparkFaultChecker;
 
 public class Manipulator extends SubsystemBase implements AutoCloseable {
-  private SparkMax pivot;
-  private SparkMax rollers;
-  private SparkMaxConfig pivotConfig;
-  private SparkMaxConfig rollerConfig;
-  private SparkClosedLoopController pivotController;
-  private SparkFaultChecker pivotChecker;
-  private SparkFaultChecker rollersChecker;
-  private AbsoluteEncoder pivotEncoder;
+  private final ManipulatorIO manipulatorIO;
+  private final ManipulatorIOInputsAutoLogged inputs = new ManipulatorIOInputsAutoLogged();
   private GameState curState = GameState.NONE;
   private Angle setpoint = Constants.reefMap.get(GameState.NONE).angle();
   private Angle error = Rotations.zero();
   private boolean atSetpoint = false;
-  private DigitalInput beamBreak;
-  private boolean beamBroken = false;
 
   public Manipulator(SparkMax pivot, SparkMax rollers) {
-    this.pivot = pivot;
-    this.rollers = rollers;
-    this.pivotConfig = new SparkMaxConfig();
-    this.rollerConfig = new SparkMaxConfig();
-    this.pivotEncoder = pivot.getAbsoluteEncoder();
-    this.pivotController = pivot.getClosedLoopController();
-    this.pivotChecker = new SparkFaultChecker(pivot, "Pivot SparkMax");
-    this.rollersChecker = new SparkFaultChecker(rollers, "Rollers SparkMax");
-    this.beamBreak = new DigitalInput(kBeamBreakPort);
-
-    pivotConfig.inverted(false).smartCurrentLimit(ManipulatorConstants.currentLimit);
-    pivotConfig.closedLoop.feedbackSensor(FeedbackSensor.kAbsoluteEncoder).pid(kP, kI, kD);
-    rollerConfig.idleMode(IdleMode.kCoast);
-
-    pivot.configure(pivotConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    rollers.configure(rollerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    manipulatorIO = new ManipulatorIOSparkMax(pivot, rollers);
   }
 
   @Override
   public void periodic() {
-    DogLog.log("manipulator/angle", getEncoderPosition().in(Units.Rotations));
-    DogLog.log("manipulator/rollers appl out", rollers.getAppliedOutput());
+    manipulatorIO.updateInputs(inputs);
     DogLog.log("manipulator/setpoint", setpoint.in(Rotations));
-    DogLog.log("manipulator/pivot appl out", pivot.getAppliedOutput());
 
-    this.error = RobotMath.dist(setpoint, getEncoderPosition());
+    this.error = RobotMath.dist(setpoint, inputs.manipulatorAngle);
     DogLog.log("manipulator/error", error.in(Rotations));
     DogLog.log("manipulator/at setpoint", atSetpoint);
-
-    this.beamBroken = beamBreak.get();
-    DogLog.log("manipulator/has coral", beamBroken);
-
-    // zeroed at 180 degrees
-    double ff = Math.sin(Math.PI - getEncoderPosition().in(Units.Radians)) * kFF;
-    DogLog.log("manipulator/ff", ff);
-    pivotController.setReference(
-        setpoint.in(Rotations),
-        ControlType.kPosition,
-        ClosedLoopSlot.kSlot0,
-        ff,
-        ArbFFUnits.kPercentOut);
-    // pivotChecker.updateFaults();
-    // rollersChecker.updateFaults();
   }
 
   private Angle getAngle(GameState state) {
@@ -95,13 +41,9 @@ public class Manipulator extends SubsystemBase implements AutoCloseable {
     this.curState = state;
     boolean reverse = Constants.reefMap.get(state).angle().gt(setpoint);
     // Speed to counteract inertial of the coral that may be in the manipulator
-    rollers.set(kWhilePivotingSpeed * (reverse ? -1 : 1));
+    manipulatorIO.setRollers(kWhilePivotingSpeed * (reverse ? -1 : 1));
     this.setpoint = getAngle(state);
     this.atSetpoint = false;
-  }
-
-  private Angle getEncoderPosition() {
-    return Rotations.of(Math.abs(pivotEncoder.getPosition()));
   }
 
   /** Check if pivot is at angle setpoint to some degree of error */
@@ -117,25 +59,23 @@ public class Manipulator extends SubsystemBase implements AutoCloseable {
     if (curState == GameState.HUMAN_PLAYER_STATION) {
       speed = kHumanPlayerStationSpeed;
     }
-    rollers.set(speed * multiplier);
+    manipulatorIO.setRollers(speed * multiplier);
   }
 
   public boolean hasCoral() {
-    return beamBroken;
+    return inputs.beamBroken;
   }
 
   public void stopRollers() {
-    rollers.set(0.0);
+    manipulatorIO.setRollers(0.0);
   }
 
   public void stop() {
-    pivot.stopMotor();
-    rollers.stopMotor();
+    manipulatorIO.stop();
   }
 
   @Override
   public void close() throws Exception {
-    pivot.close();
-    rollers.close();
+    manipulatorIO.close();
   }
 }
--- src/main/java/frc/robot/subsystems/manipulator/ManipulatorIO.java
+++ src/main/java/frc/robot/subsystems/manipulator/ManipulatorIO.java
@@ -1,24 +1,25 @@
 package frc.robot.subsystems.manipulator;
 
+import edu.wpi.first.units.measure.Angle;
 import org.littletonrobotics.junction.AutoLog;
 
-import edu.wpi.first.units.measure.Angle;
-
 public interface ManipulatorIO {
-    @AutoLog
-    public static class ManipulatorIOInputs {
-        public Angle manipulatorAngle;
-        public double rollerOutput;
-        public double pivotOutput;
+  @AutoLog
+  public static class ManipulatorIOInputs {
+    public Angle manipulatorAngle;
+    public double rollerOutput;
+    public double pivotOutput;
 
-        public boolean beamBroken;
-    }
+    public boolean beamBroken;
+  }
 
-    public default void updateInputs (ManipulatorIOInputs inputs) {}
+  public default void updateInputs(ManipulatorIOInputs inputs) {}
 
-    public default void setAngle(Angle angle) {}
+  public default void setAngle(Angle angle) {}
 
-    public default void setRollers() {}
+  public default void setRollers(double speed) {}
 
-    public default void close() {}
+  public default void stop() {}
+
+  public default void close() {}
 }
